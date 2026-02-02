b98bdf7ca27b9f737deee9e947afb047b2216502
Савелий Вотинцев <votintcev1@mail.ru>
2026-02-03 02:03:22 +0300

    Мониторинг утечек памяти
    - Добавлен таймер отслеживания потребления памяти
    - Реализовано аварийное завершение при превышении лимита

--- PoproshaykaBot.WinForms/Program.cs
+++ PoproshaykaBot.WinForms/Program.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using Timer = System.Windows.Forms.Timer;
+
 namespace PoproshaykaBot.WinForms;
 
 public static class Program
@@ -6,6 +9,48 @@ public static class Program
     private static void Main()
     {
         ApplicationConfiguration.Initialize();
+
+        var memoryCheckTimer = new Timer();
+
+        const long ThresholdMb = 1024;
+        const long MemoryThreshold = 1024 * 1024 * ThresholdMb;
+        const int Interval = 5000;
+
+        memoryCheckTimer.Interval = Interval;
+        memoryCheckTimer.Tick += (_, _) =>
+        {
+            var currentProcess = Process.GetCurrentProcess();
+            var memoryBytes = currentProcess.PrivateMemorySize64;
+
+            if (memoryBytes <= MemoryThreshold)
+            {
+                return;
+            }
+
+            memoryCheckTimer.Stop();
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(Interval);
+                currentProcess.Kill();
+            });
+
+            var memoryMb = memoryBytes / (1024.0 * 1024.0);
+            var message =
+                $"""
+                 Внимание! Обнаружено аномальное потребление ресурсов.
+
+                 Текущее использование: {memoryMb:F2} MB
+                 Установленный лимит: {ThresholdMb:F2} MB
+
+                 У вас есть {Interval / 1000} секунд, чтобы прочитать это сообщение, затем процесс будет убит принудительно.
+                 """;
+
+            MessageBox.Show(message, "Критическая нагрузка на память", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            Environment.FailFast("Пользователь подтвердил закрытие при утечке памяти.");
+        };
+
+        memoryCheckTimer.Start();
 
         using var composition = new Composition();
 
